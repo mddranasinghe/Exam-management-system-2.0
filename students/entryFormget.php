8eabfc4e49822cffcba50969c8cc014792790522
THARU527 <127031911+THARU527@users.noreply.github.com>
2023-09-12 22:23:59 +0530

    updatefont

--- students/entryFormget.php
+++ students/entryFormget.php
@@ -16,7 +16,7 @@ include "db_connection.php";
 
 
 
-<label for="Name_of_the_examination"class="col-sm-3 col-form-label" > Name of the examination &nbsp &nbsp</label>
+<label for="Name_of_the_examination"class="col-sm-3 col-form-label" > Name of the Examination &nbsp &nbsp</label>
                         <select name="Name_of_the_examination" id="Name_of_the_examination" class="form-control "required>
                                     <option value="">Select The Exam</option>
                                     <option value="First Year First Semester Examination In Information Communication Technology">First Year First Semester Examination In Information Communication Technology</option>
@@ -25,22 +25,22 @@ include "db_connection.php";
                                     <option value="Second Year Second Semester Examination In Information Communication Technology">Second Year Second Semester Examination In Information Communication Technology</option>
                                     <option value="Third Year First Semester Examination In Information Communication Technology">Third Year First Semester Examination In Information Communication Technology</option>
                                     <option value="Third Year Second Semester Examination In Information Communication Technology">Third Year Second Semester Examination In Information Communication Technology</option>
-                                    <option value="Forth Year First Semester Examination In Information Communication Technology">Forth Year First Semester Examination In Information Communication Technology</option>
-                                    <option value="Forth Year Second Semester Examination In Information Communication Technology">Forth Year Second Semester Examination In Information Communication Technology</option>
+                                    <option value="Forth Year First Semester Examination In Information Communication Technology">Fourth Year First Semester Examination In Information Communication Technology</option>
+                                    <option value="Forth Year Second Semester Examination In Information Communication Technology">Fourth Year Second Semester Examination In Information Communication Technology</option>
                                 </select>
                      
   
                                 <label for="Faculty of" class="col-s m-2 col-form-label"> Faculty of</label>
                             <select name="faculty" id="faculty"  class="form-control col-s m-2 col-form-label" required>
                                     <option value="">Select faculty &nbsp</option>
-                                    <option value="Technological studies">Technological studies</option>
-                                    <option value="applied sceince">applied sceince</option>
-                                    <option value="manegment">manegment</option>
+                                    <option value="Technological studies">Technological Studies</option>
+                                    <option value="applied sceince">Applied Sceince</option>
+                                    <option value="manegment">Business Studies</option>
                                 </select>
             
-                        <label for="year_of_the_examination" class="col-s m-2 col-form-label"> year of the examination &nbsp  </label>
+                        <label for="year_of_the_examination" class="col-s m-2 col-form-label"> Year of the Examination &nbsp  </label>
                                 <select name="year" id="year"  class="form-control col-s m-2 col-form-label" required>
-                                    <option value="">Select year</option>
+                                    <option value="">Select Year</option>
                                     <option value="1st year">1st year</option>
                                     <option value="2nd year">2nd year</option>
                                     <option value="3rd year">3rd year</option>
@@ -52,7 +52,7 @@ include "db_connection.php";
                             
                         <label for="Semester" class="col-s m-2 col-form-label"> Semester &nbsp </label>
                                 <select name="semester" id="language"    class="form-control col-s m-2 col-form-label" required>
-                                    <option value="">Select semester</option>
+                                    <option value="">Select Semester</option>
                                     <option value="1st semester">1st semester</option>
                                     <option value="2nd semester">2nd semester</option>
                                 </select>
