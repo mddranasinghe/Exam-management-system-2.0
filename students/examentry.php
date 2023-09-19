982eff125a5af4dcba2bfb1d25442e7547f04f06
Sachintha-sandaruwan-rajapakshe <126498862+Sachintha-sandaruwan-rajapakshe@users.noreply.github.com>
2023-09-20 00:11:13 +0530

    Update examentry.php

--- students/examentry.php
+++ students/examentry.php
@@ -517,12 +517,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     if(isset($_POST['submit']))
                     {
                         $count=0;
-                        $sql="SELECT Registration_No from `examEnrty`";
+                        $sql="SELECT Registration_No,Name_of_the_examination from `examEnrty`";
                         $res=mysqli_query($conn,$sql);
 
                         while($row=mysqli_fetch_assoc($res))
                         {
-                        if($row['Registration_No']==$_POST['Registration_No'])
+                        if($row['Registration_No']==$_POST['Registration_No']&& $row['Name_of_the_examination']==$_POST['Name_of_the_examination'])
                         {
                             $count=$count+1;
                         }
